42e6c6e2652511f90839747e96988fe8b202c7cc
Luke McCulloch <tlukemcculloch@gmail.com>
2019-02-20 12:56:12 -0600

    Update JacobiP.py

--- JacobiP.py
+++ JacobiP.py
@@ -5,6 +5,9 @@ Created on Sat Feb 16 00:03:10 2019
 
 @author: lukemcculloch
 """
+import numpy as np
+from scipy.special import gamma #, factorial
+sqrt = np.sqrt
 
 def JacobiP(x,alpha,beta,N):
     """
@@ -14,33 +17,47 @@ def JacobiP(x,alpha,beta,N):
                 returns P[1:length(xp))]
     - Note   : They are normalized to be orthonormal.
     #"""
+    #safety first, a hem:
+    aold=0.0
+    anew=0.0
+    bnew=0.0
+    h1=0.0
+    gamma0=0.0
+    gamma1=0.0
     # Turn points into row if needed.
-    xp = x; dims = size(xp);
-    if (dims(2)==1) xp = xp'; end;
+    xp = x
+    dims = np.shape(xp)
+    if (dims(1)==1):
+        xp = xp.conj().transpose()
     
-    PL = zeros(N+1,length(xp)); 
+    
+    PL = np.zeros(N+1,np.size(xp))
     
     # Initial values P_0(x) and P_1(x)
-    gamma0 = 2^(alpha+beta+1)/(alpha+beta+1)*gamma(alpha+1)*...
-        gamma(beta+1)/gamma(alpha+beta+1);
-    PL(1,:) = 1.0/sqrt(gamma0);
-    if (N==0) P=PL'; return; end;
-    gamma1 = (alpha+1)*(beta+1)/(alpha+beta+3)*gamma0;
-    PL(2,:) = ((alpha+beta+2)*xp/2 + (alpha-beta)/2)/sqrt(gamma1);
-    if (N==1) P=PL(N+1,:)'; return; end;
+    gamma0 = 2**(alpha+beta+1)/(alpha+beta+1)*gamma[alpha+1]* \
+        gamma[beta+1]/gamma[alpha+beta+1]
+        
+    PL[1,:] = 1.0/sqrt(gamma0)
+    if (N==0): 
+        P=PL.conj().transpose()
+        return
+    gamma1 = (alpha+1)*(beta+1)/(alpha+beta+3)*gamma0
+    PL[2,:] = ((alpha+beta+2)*xp/2 + (alpha-beta)/2)/sqrt(gamma1)
+    if (N==1): 
+        P=PL[N+1,:].conj().transpose()
+        return
     
+    #TLM current spot:
     # Repeat value in recurrence.
-    aold = 2/(2+alpha+beta)*sqrt((alpha+1)*(beta+1)/(alpha+beta+3));
+    aold = 2/(2+alpha+beta)*sqrt((alpha+1)*(beta+1)/(alpha+beta+3))
     
     # Forward recurrence using the symmetry of the recurrence.
-    for i=1:N-1
-      h1 = 2*i+alpha+beta;
-      anew = 2/(h1+2)*sqrt( (i+1)*(i+1+alpha+beta)*(i+1+alpha)*...
-          (i+1+beta)/(h1+1)/(h1+3));
-      bnew = - (alpha^2-beta^2)/h1/(h1+2);
-      PL(i+2,:) = 1/anew*( -aold*PL(i,:) + (xp-bnew).*PL(i+1,:));
-      aold =anew;
-    end;
+    for i in range(N-1):
+        h1 = 2*i+alpha+beta
+        anew = 2/(h1+2)*sqrt( (i+1)*(i+1+alpha+beta)*(i+1+alpha)* \
+          (i+1+beta)/(h1+1)/(h1+3))
+        bnew = - (alpha**2-beta**2)/h1/(h1+2)
+        aold =anew
     
-    P = PL(N+1,:)';
-    return
+    P = PL[N+1,:].conj().transpose()
+    return P
